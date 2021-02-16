6049b4d2d53ae512916bca624d22ff70ff07df2c
Ilya Straumit <ilya.straumit@kuleuven.be>
2021-02-16 11:10:28 +0100

    moving GSSP step settings to common.py

--- common.py
+++ common.py
@@ -1,2 +1,12 @@
 param_names = ['T_eff','log(g)','v*sin(i)','v_micro','[M/H]']
 param_units = ['[K]','[cm/s^2]','[km/s]','[km/s]','[dex]']
+
+GSSP_steps = {}
+GSSP_steps['T_eff'] = [10000, 100, 250]
+GSSP_steps['log(g)'] = [0.1]
+GSSP_steps['v_micro'] = [0.5]
+GSSP_steps['[M/H]'] = [0.1]
+GSSP_steps['v*sin(i)'] = [10]
+
+assert all([key in param_names for key in GSSP_steps])
+assert all([key in GSSP_steps for key in param_names])
--- random_grid_common.py
+++ random_grid_common.py
@@ -17,15 +17,6 @@ def parse_inp():
     return opt
 
 
-GSSP_steps = {}
-GSSP_steps['T_eff'] = [10000, 100, 250]
-GSSP_steps['log(g)'] = [0.1]
-GSSP_steps['v_micro'] = [0.5]
-GSSP_steps['[M/H]'] = [0.1]
-GSSP_steps['v*sin(i)'] = [10]
-
-assert all([key in param_names for key in GSSP_steps])
-assert all([key in GSSP_steps for key in param_names])
 
 
 
